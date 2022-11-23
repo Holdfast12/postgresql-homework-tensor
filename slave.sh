7ca187d3471c46845bff6985a86da0eab6e66f36
Калюжный Михаил Сергеевич <mkalyuzhnyj@office.er76.ru>
2022-11-23 13:12:25 +0300

    123

--- slave.sh
+++ slave.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+sudo pg_basebackup -R -D /var/lib/pgsql/13/data/ -h 192.168.1.2 -U postgres -P -v
+sudo chown -R 26:26 /var/lib/pgsql/13/data/
+sudo systemctl enable postgresql-13 --now
